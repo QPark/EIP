0c5aa167f3689c6587ad524bf1372da4bf2e6909
Bernhard Hausen <bernhard.hausen@gmx.net>
2015-10-12 10:15:49 +0200

    Fixed dependencies

--- qp-enterprise-integration-platform-impl/eip-core/src/main/java/com/qpark/eip/core/DBUtil.java
+++ qp-enterprise-integration-platform-impl/eip-core/src/main/java/com/qpark/eip/core/DBUtil.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2013 QPark Consulting  S.a r.l.
- * 
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0. 
- * The Eclipse Public License is available at 
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0.
+ * The Eclipse Public License is available at
  * http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Bernhard Hausen - Initial API and implementation
  *
@@ -24,76 +24,77 @@ import javax.persistence.TypedQuery;
  * @author bhausen
  */
 public abstract class DBUtil {
-	/**
-	 * Set the maximum result set of the {@link TypedQuery}.
-	 * @param typedQuery The {@link TypedQuery}
-	 * @param maxElements The maximum number of elements if given.
-	 */
-	public static void setMaxElements(final TypedQuery<?> typedQuery,
-			final BigInteger maxElements, final Map<String, String> properties) {
-		if (typedQuery != null) {
-			int max = EipSettings.EIP_UTIL_MAX_DATABASE_LINES_NUMBER;
-			if (maxElements != null && maxElements.intValue() > 0) {
-				max = maxElements.intValue();
-			} else if (properties
-					.get(EipSettings.EIP_UTIL_MAX_DATABASE_LINES) != null) {
-				try {
-					max = Integer.parseInt(properties
-							.get(EipSettings.EIP_UTIL_MAX_DATABASE_LINES));
-				} catch (NumberFormatException e) {
-					// nothing to do.
-				}
-			}
-			typedQuery.setMaxResults(max);
-		}
-	}
-
-	/**
-	 * @param url
-	 * @param username
-	 * @param password
-	 * @return the connection or null.
-	 */
-	private static Connection getOracleConnection(final String url,
-			final String username, final String password) {
-		Connection connection = null;
+    /**
+     * Set the maximum result set of the {@link javax.persistence.TypedQuery}.
+     *
+     * @param typedQuery
+     *            The {@link TypedQuery}
+     * @param maxElements
+     *            The maximum number of elements if given.
+     */
+    public static void setMaxElements(final TypedQuery<?> typedQuery, final BigInteger maxElements,
+	    final Map<String, String> properties) {
+	if (typedQuery != null) {
+	    int max = EipSettings.EIP_UTIL_MAX_DATABASE_LINES_NUMBER;
+	    if (maxElements != null && maxElements.intValue() > 0) {
+		max = maxElements.intValue();
+	    } else if (properties.get(EipSettings.EIP_UTIL_MAX_DATABASE_LINES) != null) {
 		try {
-			String driverName = "oracle.jdbc.driver.OracleDriver";
-			Class.forName(driverName);
-			connection = DriverManager.getConnection(url, username, password);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
+		    max = Integer.parseInt(properties.get(EipSettings.EIP_UTIL_MAX_DATABASE_LINES));
+		} catch (NumberFormatException e) {
+		    // nothing to do.
 		}
-		return connection;
+	    }
+	    typedQuery.setMaxResults(max);
 	}
+    }
 
-	/**
-	 * If the criteria does not start or end with <code>*</code> it will appended. Then the method
-	 * replaces all occurrences of <code>?</code> by <code>_</code> and <code>*</code> by
-	 * <code>%</code>.
-	 * @param criteria the criteria.
-	 * @return the changed criteria.
-	 */
-	public static String prepareContainsSearch(final String criteria) {
-		String s = criteria;
-		if (s != null) {
-			s = s.trim();
-			if (s.length() == 0) {
-				s = null;
-			} else {
-				if (!s.startsWith("*")) {
-					s = new StringBuffer(s.length() + 1).append('*').append(s)
-							.toString();
-				}
-				if (!s.endsWith("*")) {
-					s = new StringBuffer(s).append('*').toString();
-				}
-				s = s.replaceAll("\\?", "_");
-				s = s.replaceAll("\\*", "%");
-			}
-		}
-		return s;
+    /**
+     * @param url
+     * @param username
+     * @param password
+     * @return the connection or null.
+     */
+    private static Connection getOracleConnection(final String url, final String username, final String password) {
+	Connection connection = null;
+	try {
+	    String driverName = "oracle.jdbc.driver.OracleDriver";
+	    Class.forName(driverName);
+	    connection = DriverManager.getConnection(url, username, password);
+	} catch (ClassNotFoundException e) {
+	    e.printStackTrace();
+	} catch (SQLException e) {
+	    e.printStackTrace();
 	}
+	return connection;
+    }
+
+    /**
+     * If the criteria does not start or end with <code>*</code> it will
+     * appended. Then the method replaces all occurrences of <code>?</code> by
+     * <code>_</code> and <code>*</code> by <code>%</code>.
+     *
+     * @param criteria
+     *            the criteria.
+     * @return the changed criteria.
+     */
+    public static String prepareContainsSearch(final String criteria) {
+	String s = criteria;
+	if (s != null) {
+	    s = s.trim();
+	    if (s.length() == 0) {
+		s = null;
+	    } else {
+		if (!s.startsWith("*")) {
+		    s = new StringBuffer(s.length() + 1).append('*').append(s).toString();
+		}
+		if (!s.endsWith("*")) {
+		    s = new StringBuffer(s).append('*').toString();
+		}
+		s = s.replaceAll("\\?", "_");
+		s = s.replaceAll("\\*", "%");
+	    }
+	}
+	return s;
+    }
 }
--- qp-enterprise-integration-platform-impl/eip-core/src/main/java/com/qpark/eip/core/EagerLoader.java
+++ qp-enterprise-integration-platform-impl/eip-core/src/main/java/com/qpark/eip/core/EagerLoader.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2013 QPark Consulting  S.a r.l.
- * 
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0. 
- * The Eclipse Public License is available at 
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0.
+ * The Eclipse Public License is available at
  * http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Bernhard Hausen - Initial API and implementation
  *
@@ -13,62 +13,82 @@
 package com.qpark.eip.core;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.TreeMap;
-
-import org.hibernate.Hibernate;
 
 import com.qpark.eip.core.PropertyDescriptorUtil.ObjectProperties;
 
+/**
+ * Loads complete objects via JPA.
+ *
+ * @see http://stackoverflow.com/questions/15359306/how-to-load-lazy-fetched-
+ *      items-from-hibernate-jpa-in-my-controller
+ * @author bhausen
+ */
 public class EagerLoader {
-	private static final Object[] EMPTY = new Object[] {};
+    private static final Object[] EMPTY = new Object[] {};
 
-	public static void loadCollection(final Collection<Object> os) {
-		for (Object o : os) {
-			load(o);
-		}
+    /**
+     * You will have to make an explicit call on the lazy collection in order to
+     * initialize it (common practice is to call .size() for this purpose). In
+     * Hibernate there is a dedicated method for this (Hibernate.initialize()),
+     * but JPA has no equivalent of that. Of course you will have to make sure
+     * that the invokation is done, when the session is still available, so
+     * annotate your controller method with @Transactional. An alternative is to
+     * create an intermediate Service layer between the Controller and the
+     * Repository that could expose methods which initialize lazy collections.
+     *
+     * @param os
+     */
+    public static void loadCollection(final Collection<?> os) {
+	os.size();
+	for (Object o : os) {
+	    load(o);
 	}
+    }
 
-	public static void loadArray(final Object... os) {
-		for (Object o : os) {
-			load(o);
-		}
+    /**
+     * Load each {@link Object} out of the array of {@link Object}s.
+     *
+     * @param os
+     */
+    public static void loadArray(final Object... os) {
+	for (Object o : os) {
+	    load(o);
 	}
+    }
 
-	public static void load(final Object o) {
-		if (o != null) {
-			if (List.class.isInstance(o)) {
-				loadCollection((Collection) o);
-			} else if (Collection.class.isInstance(o)) {
-				loadCollection((Collection) o);
-			} else if (o.getClass().isArray()) {
-				loadArray((Object[]) o);
-			} else {
-				Hibernate.initialize(o);
-				TreeMap<String, ObjectProperties> pdos = PropertyDescriptorUtil
-						.getObjectProperties(o);
-				Object property;
-				for (ObjectProperties pdx : pdos.values()) {
-					try {
-						property = pdx.read.invoke(o, EMPTY);
-						if (property != null) {
-							if (!pdx.type.getPackage().getName()
-									.startsWith("java")) {
-								load(property);
-							} else if (List.class.isInstance(property)) {
-								load(property);
-							} else if (Collection.class.isInstance(property)) {
-								load(property);
-							} else if (property.getClass().isArray()) {
-								load(property);
-							}
-						}
-					} catch (Exception e) {
-						// nothing to do.
-					}
-				}
+    /**
+     * Load each property of the {@link Object}.
+     * 
+     * @param o
+     *            the {@link Object}.
+     */
+    public static void load(final Object o) {
+	if (o != null) {
+	    if (Collection.class.isInstance(o)) {
+		loadCollection((Collection<?>) o);
+	    } else if (o.getClass().isArray()) {
+		loadArray((Object[]) o);
+	    } else {
+		TreeMap<String, ObjectProperties> pdos = PropertyDescriptorUtil.getObjectProperties(o);
+		Object property;
+		for (ObjectProperties pdx : pdos.values()) {
+		    try {
+			property = pdx.read.invoke(o, EMPTY);
+			if (property != null) {
+			    if (!pdx.type.getPackage().getName().startsWith("java")) {
+				load(property);
+			    } else if (Collection.class.isInstance(property)) {
+				load(property);
+			    } else if (property.getClass().isArray()) {
+				load(property);
+			    }
 			}
+		    } catch (Exception e) {
+			// nothing to do.
+		    }
 		}
+	    }
 	}
-
+    }
 }
