88602634f3c9e373e68a968b5fc44e273f8c93a2
bhausen <bernhard.hausen@gmx.net>
2016-09-14 09:35:50 +0200

    DomainDoc: Model update

--- qp-enterprise-integration-platform/eip-model-analysis/src/main/java/com/qpark/eip/model/docmodel/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.11 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2016.09.14 um 06:01:36 AM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.qpark-consulting.com/EIP/Utility/DocumentationModel")
-package com.qpark.eip.model.docmodel;
