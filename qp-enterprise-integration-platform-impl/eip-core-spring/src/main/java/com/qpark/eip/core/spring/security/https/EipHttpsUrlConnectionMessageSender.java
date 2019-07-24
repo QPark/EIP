4e05639209fe877307e1bde60684c37c55b20c91
bhausen <bernhard.hausen@gmx.net>
2019-07-24 08:05:21 +0200

    Set ContentType for HTTPs Message sender

--- qp-enterprise-integration-platform-impl/eip-core-spring/src/main/java/com/qpark/eip/core/spring/security/https/EipHttpsUrlConnectionMessageSender.java
+++ qp-enterprise-integration-platform-impl/eip-core-spring/src/main/java/com/qpark/eip/core/spring/security/https/EipHttpsUrlConnectionMessageSender.java
@@ -1,9 +1,9 @@
 /*******************************************************************************
  * Copyright (c) 2013, 2014, 2015 QPark Consulting  S.a r.l.
- * 
- * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0. 
- * The Eclipse Public License is available at 
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0.
+ * The Eclipse Public License is available at
  * http://www.eclipse.org/legal/epl-v10.html.
  ******************************************************************************/
 package com.qpark.eip.core.spring.security.https;
@@ -11,9 +11,11 @@ package com.qpark.eip.core.spring.security.https;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
-
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
@@ -25,99 +27,132 @@ import org.springframework.ws.transport.http.HttpsUrlConnectionMessageSender;
 /**
  * @author bhausen
  */
-public class EipHttpsUrlConnectionMessageSender extends HttpsUrlConnectionMessageSender {
-    /** The user name of the basic HTTP-authentication. */
-    private String userName;
+public class EipHttpsUrlConnectionMessageSender
+		extends HttpsUrlConnectionMessageSender {
+	/** The user name of the basic HTTP-authentication. */
+	private String userName;
+	/** The password of the basic HTTP-authentication. */
+	private String password;
+	/** The HTTP request content type. */
+	private String contentType;
+	/** The {@link X509TrustManager} using the keystore.jks. */
+	@Autowired
+	private EipX509TrustManager x509TrustManager;
+	/** base64(userName:password) */
+	private String base64UserNamePassword;
 
-    /** The password of the basic HTTP-authentication. */
-    private String password;
-
-    /** The {@link X509TrustManager} using the keystore.jks. */
-    @Autowired
-    private EipX509TrustManager x509TrustManager;
-
-    /** base64(userName:password) */
-    private String base64UserNamePassword;
-
-    /**
-     * @return the password.
-     */
-    public String getPassword() {
-	return this.password;
-    }
-
-    /**
-     * @return the userName.
-     */
-    public String getUserName() {
-	return this.userName;
-    }
-
-    /**
-     * @return the x509TrustManager.
-     */
-    public EipX509TrustManager getX509TrustManager() {
-	return this.x509TrustManager;
-    }
-
-    /**
-     * Initialize the HTTPs conncection.
-     * 
-     * @throws UnsupportedEncodingException
-     */
-    @PostConstruct
-    public void init() throws UnsupportedEncodingException {
-	if (this.userName != null) {
-	    if (this.password == null) {
-		this.password = "";
-	    }
-	    this.base64UserNamePassword = new String(Base64.encodeBase64String(
-		    new StringBuffer(this.userName).append(":").append(this.password).toString().getBytes("UTF-8")))
-			    .replaceAll("\n", "");
+	/**
+	 * @return the contentType.
+	 */
+	public String getContentType() {
+		return this.contentType;
 	}
-    }
 
-    /**
-     * @see org.springframework.ws.transport.http.HttpsUrlConnectionMessageSender#prepareConnection(java.net.HttpURLConnection)
-     */
-    @Override
-    protected void prepareConnection(final HttpURLConnection connection) throws IOException {
-	if (HttpsURLConnection.class.isInstance(connection)) {
-	    this.setHostnameVerifier(this.x509TrustManager);
-	    this.setTrustManagers(new TrustManager[] { this.x509TrustManager });
+	/**
+	 * @return the password.
+	 */
+	public String getPassword() {
+		return this.password;
 	}
-	/* call the super method. */
-	super.prepareConnection(connection);
 
-	/* Setup the basic Authentication. */
-	if (HttpURLConnection.class.isInstance(connection) && this.userName != null) {
-	    HttpURLConnection httpsConnection = connection;
-	    httpsConnection.setRequestProperty("Authorization",
-		    new StringBuffer(128).append("Basic ").append(this.base64UserNamePassword).toString());
+	/**
+	 * @return the userName.
+	 */
+	public String getUserName() {
+		return this.userName;
 	}
-    }
 
-    /**
-     * @param password
-     *            the password to set.
-     */
-    public void setPassword(final String password) {
-	this.password = password;
-    }
+	/**
+	 * @return the x509TrustManager.
+	 */
+	public EipX509TrustManager getX509TrustManager() {
+		return this.x509TrustManager;
+	}
 
-    /**
-     * @param userName
-     *            the userName to set.
-     */
-    public void setUserName(final String userName) {
-	this.userName = userName;
-    }
+	/**
+	 * Initialize the HTTPs conncection.
+	 *
+	 * @throws UnsupportedEncodingException
+	 */
+	@PostConstruct
+	public void init() throws UnsupportedEncodingException {
+		if (this.userName != null) {
+			if (this.password == null) {
+				this.password = "";
+			}
+			this.base64UserNamePassword = new String(
+					Base64.encodeBase64String(new StringBuffer(this.userName)
+							.append(":").append(this.password).toString()
+							.getBytes("UTF-8"))).replaceAll("\n", "");
+		}
+	}
 
-    /**
-     * @param x509TrustManager
-     *            the x509TrustManager to set.
-     */
-    public void setX509TrustManager(final EipX509TrustManager x509TrustManager) {
-	this.x509TrustManager = x509TrustManager;
-    }
+	/**
+	 * @see org.springframework.ws.transport.http.HttpsUrlConnectionMessageSender#prepareConnection(java.net.HttpURLConnection)
+	 */
+	@Override
+	protected void prepareConnection(final HttpURLConnection connection)
+			throws IOException {
+		if (HttpsURLConnection.class.isInstance(connection)) {
+			this.setHostnameVerifier(this.x509TrustManager);
+			this.setTrustManagers(new TrustManager[] { this.x509TrustManager });
+		}
+		/* call the super method. */
+		super.prepareConnection(connection);
+
+		/* Setup the basic Authentication. */
+		if (Objects.nonNull(this.userName)) {
+			this.logger.debug(String.format(
+					"prepareConnection add request header '%s' basic AUTH userName '%s'",
+					"Authorization", this.userName));
+			connection.setRequestProperty("Authorization",
+					new StringBuffer(128).append("Basic ")
+							.append(this.base64UserNamePassword).toString());
+			this.logger.debug(String.format(
+					"prepareConnection request Headers: %s",
+					connection.getRequestProperties().entrySet().stream()
+							.sorted(Comparator
+									.comparing(v -> String.valueOf(v.getKey())))
+							.map(v -> String.format("%s: %s", v.getKey(),
+									v.getValue().stream()
+											.collect(Collectors.joining(","))))
+							.collect(Collectors.joining(", "))));
+		}
+		/* Setup ContentType HTTP header. */
+		if (Objects.nonNull(this.contentType)) {
+			connection.setRequestProperty("Content-Type", this.contentType);
+		}
+	}
+
+	/**
+	 * @param contentType
+	 */
+	public void setContentType(final String contentType) {
+		this.contentType = contentType;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set.
+	 */
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set.
+	 */
+	public void setUserName(final String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @param x509TrustManager
+	 *            the x509TrustManager to set.
+	 */
+	public void setX509TrustManager(
+			final EipX509TrustManager x509TrustManager) {
+		this.x509TrustManager = x509TrustManager;
+	}
 }
