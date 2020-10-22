72ffb72686bbcf16bccc06bed5957ecb7bac7739
bhausen <bernhard.hausen@gmx.net>
2020-10-22 16:50:44 +0200

    Added standard decryption method

--- qp-enterprise-integration-platform-impl/eip-core-spring/src/main/java/com/qpark/eip/core/spring/security/EipJasyptEncryptionProvider.java
+++ qp-enterprise-integration-platform-impl/eip-core-spring/src/main/java/com/qpark/eip/core/spring/security/EipJasyptEncryptionProvider.java
@@ -8,6 +8,7 @@
 package com.qpark.eip.core.spring.security;
 
 import java.util.Optional;
+import org.jasypt.exceptions.EncryptionOperationNotPossibleException;
 import org.jasypt.util.text.StrongTextEncryptor;
 import com.qpark.eip.core.spring.ApplicationPlaceholderConfigurer;
 
@@ -19,14 +20,22 @@ public class EipJasyptEncryptionProvider {
   public static String EIP_ENCRYPTOR_PWD_PROPERTY_NAME = "eip.jasypt.encryptor.password";
 
   /**
-   * Get the encryptor password from Environment, system properties or properties.
+   * Encrypts the text if it starts with <i>ENC(</i> and ends with <i>)</i>.
+   * @param text the text to encrypt.
    * @param properties the {@link ApplicationPlaceholderConfigurer}.
-   * @return the password.
+   * @return the encrypted text.
+   * @throws EncryptionOperationNotPossibleException
    */
-  public static String getEncryptorPassword(final ApplicationPlaceholderConfigurer properties) {
-    return Optional.ofNullable(System.getenv(EIP_ENCRYPTOR_PWD_PROPERTY_NAME))
-        .orElse(System.getProperty(EIP_ENCRYPTOR_PWD_PROPERTY_NAME,
-            properties.getProperty(EIP_ENCRYPTOR_PWD_PROPERTY_NAME, "eip")));
+  public static String decrypt(final String text, final ApplicationPlaceholderConfigurer properties)
+      throws EncryptionOperationNotPossibleException {
+    return Optional.ofNullable(text).map(t -> {
+      String value = null;
+      if (t.trim().startsWith("ENC(") && t.trim().endsWith(")")) {
+        value =
+            getEncryptor(properties).decrypt(t.substring(0, t.length() - 1).replace("ENC(", ""));
+      }
+      return value;
+    }).orElse(text);
   }
 
   /**
@@ -38,5 +47,16 @@ public class EipJasyptEncryptionProvider {
     final StrongTextEncryptor encryptor = new StrongTextEncryptor();
     encryptor.setPassword(getEncryptorPassword(properties));
     return encryptor;
+  }
+
+  /**
+   * Get the encryptor password from Environment, system properties or properties.
+   * @param properties the {@link ApplicationPlaceholderConfigurer}.
+   * @return the password.
+   */
+  public static String getEncryptorPassword(final ApplicationPlaceholderConfigurer properties) {
+    return Optional.ofNullable(System.getenv(EIP_ENCRYPTOR_PWD_PROPERTY_NAME))
+        .orElse(System.getProperty(EIP_ENCRYPTOR_PWD_PROPERTY_NAME, Optional.ofNullable(properties)
+            .map(p -> p.getProperty(EIP_ENCRYPTOR_PWD_PROPERTY_NAME, "eip")).orElse("eip")));
   }
 }
